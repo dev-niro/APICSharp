4f63f9e9e922738ab71387cbcbd1030e0dc304d3
dev-niro <n.moyae@duocuc.cl>
2024-06-01 00:14:35 -0400

    Initial commit

---
+++ Usuarios/Usuarios.API/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Usuarios.API
+{
+	public class Cliente
+	{
+		public int Id { get; set; }
+		
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string Email { get; set; }
+
+	}
+}
---
+++ Usuarios/Usuarios.API/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Usuarios.API
+{
+	public class Producto
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public int Price { get; set; }
+
+		public string Description { get; set; }
+
+		public Boolean InStock { get; set; }
+
+
+	}
+}
